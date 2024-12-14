466f582cf2d91da30df8c81f4d706fcd60fb00ce
Marcos Alejandro Collazos Marmolejo <112360288+Archimarks@users.noreply.github.com>
2024-12-14 11:51:13 -0500

    Update AutoInstall.sh

--- AutoInstall.sh
+++ AutoInstall.sh
@@ -35,6 +35,20 @@ else
     exit 1
 fi
 
+# Dar permisos de ejecución a todos los elementos en ~/.config/KaliDesign/
+echo "Otorgando permisos de ejecución a todos los elementos en ~/.config/KaliDesign/"
+
+find ~/.config/KaliDesign/ -type f -exec chmod +x {} \;
+find ~/KaliDesign/Elementos -type f -exec chmod +x {} \;
+
+if [ $? -eq 0 ]; then
+    echo "Permisos de ejecución asignados con éxito."
+else
+    echo "Hubo un error al asignar permisos de ejecución."
+    exit 1
+fi
+
+
 # Verificar y limpiar configuración anterior del panel
 if [ -d "~/.config/xfce4/panel/" ]; then
     rm -f ~/.config/xfce4/panel/*
@@ -53,5 +67,21 @@ if command -v xfce4-panel >/dev/null 2>&1; then
     xfce4-panel --restart
 else
     echo "El comando xfce4-panel no está disponible. Asegúrate de estar usando XFCE."
+    exit 1
+fi
+
+# Establecer el fondo de pantalla
+WALLPAPER_PATH="$HOME/KaliDesign/Elementos/Wallpapers/wallpaper1.jpg"
+
+if [ -f "$WALLPAPER_PATH" ]; then
+    xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s "$WALLPAPER_PATH"
+    if [ $? -eq 0 ]; then
+        echo "Fondo de pantalla configurado exitosamente."
+    else
+        echo "Error al configurar el fondo de pantalla."
+        exit 1
+    fi
+else
+    echo "El archivo $WALLPAPER_PATH no existe. Abortando."
     exit 1
 fi
